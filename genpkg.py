a22c035026e3ec925a91bc2b2d307fe9c204d6fc
Evan Laske <evan.laske@gmail.com>
2014-04-25 00:09:46 -0500

    Adding a bunch more notes and files concerning Atlassian's download pages

---
+++ genpkg.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Author: Evan Laske
+# @Date:   2014-04-07 23:21:58
+# @Last Modified by:   evan
+# @Last Modified time: 2014-04-24 23:49:51
+
+import urllib2
+
+
+# Standard main call
+if __name__ == "__main__":
+    main()
