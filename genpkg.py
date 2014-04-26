739663cdf69f9cf5ac0d2e6c6d0a841f5489e593
Evan Laske <evan.laske@gmail.com>
2014-04-26 01:11:29 -0400

    Added chunk reading / status functions

--- genpkg.py
+++ genpkg.py
@@ -3,10 +3,11 @@
 # @Author: Evan Laske
 # @Date:   2014-04-07 23:21:58
 # @Last Modified by:   Evan Laske
-# @Last Modified time: 2014-04-25 23:46:00
+# @Last Modified time: 2014-04-26 01:06:00
 
 import urllib2
 import json
+import sys
 
 def getURL(feed):
     base_url = 'https://my.atlassian.com/download/feeds/%s/jira.json'
@@ -27,10 +28,48 @@ def getDownload(download_data):
 def getDownloadURL(download):
     return download['zipUrl']
 
+def chunk_report(bytes_so_far, chunk_size, total_size):
+    percent = float(bytes_so_far) / total_size
+    percent = round(percent*100, 2)
+    # Write the status of the download out - keep it on the same line
+    # Note: This doesn't work in the Sublime built-in terminal
+    sys.stdout.write("Downloaded %d of %d bytes (%0.2f%%)\r" % (bytes_so_far, total_size, percent))
+    sys.stdout.flush()
+
+    # If we reached the end of the download, finally move to the next line
+    if bytes_so_far >= total_size:
+        sys.stdout.write('\n')
+
+def chunk_read(response, chunk_size=8192, report_hook=None):
+    total_size = response.info().getheader('Content-Length').strip()
+    total_size = int(total_size)
+    bytes_so_far = 0
+
+    while 1:
+        chunk = response.read(chunk_size)
+        bytes_so_far += len(chunk)
+
+        if not chunk:
+            break
+
+        if report_hook:
+            report_hook(bytes_so_far, chunk_size, total_size)
+
+    return bytes_so_far
+
+def downloadTAR(url):
+    response = urllib2.urlopen(url)
+    #chunk_read(response, report_hook=chunk_report)
+    print response.info()
+    print response.info().getheader('Content-Length')
+    response.read()
+    print 'Done read()'
+
 def main():
     print getURL('current')
     print getDownload(getFeed(getURL('current')))
-    print getDownloadURL(getDownload(getFeed(getURL('current'))))
+    url = getDownloadURL(getDownload(getFeed(getURL('current'))))
+    downloadTAR(url)
 
 # Standard main call
 if __name__ == "__main__":
